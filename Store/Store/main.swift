3ddb126e449a7742af15e799de8eb8ad959dd98a
Ted Neward <ted@tedneward.com>
2024-03-27 18:19:08 -0700

    Committing file shells

---
+++ Store/Store/main.swift
@@ -0,0 +1,24 @@
+//
+//  main.swift
+//  Store
+//
+//  Created by Ted Neward on 2/29/24.
+//
+
+import Foundation
+
+protocol SKU {}
+
+class Item {}
+
+class Receipt {}
+
+class Register {}
+
+class Store {
+    let version = "0.1"
+    func helloWorld() -> String {
+        return "Hello world"
+    }
+}
+
